c8eafbe888f6d953e412c0f56c171feb549a2d48
jim3692 <jim3692@gmail.com>
2022-04-23 16:49:10 +0300

    Tests bundling + Beautify

--- bundle.sh
+++ bundle.sh
@@ -9,7 +9,7 @@ fi
 
 PROJECT_ROOT=$PWD
 CLASSES_PATH='.build/main/default/classes'
-CLASS_REGEX="(\d|\w|_)+"
+CLASS_REGEX="[A-Z](\d|\w|_)+"
 
 [ -d .build ] && rm -rf .build
 cp -r src .build
@@ -24,10 +24,17 @@ function scanClasses () {
   find -not -path './externals/*' -name '*.cls'
 }
 
+function scanTestExternals () {
+  find -path './tests/externals/*' -name '*.cls'
+}
+
+function scanTestClasses () {
+  find -maxdepth 2 -path './tests/*' -name '*.cls'
+}
+
 function nameOfClass () {
   while read -r line; do
     echo $line | sed -E "s/^.*\/($CLASS_REGEX).cls$/\1/"
-    # echo $line | egrep -o "$CLASS_REGEX.cls$" | egrep -o "^$CLASS_REGEX"
   done
 }
 
@@ -50,7 +57,6 @@ function fixBaseClass () {
 
   foreignExternalInitCalls $baseClassPath
   externalInitCalls $baseClassPath
-  prepareBaseClassForInjection
 }
 
 function foreignExternalInitCalls () {
@@ -109,11 +115,12 @@ function externalInitCalls () {
   fi
 }
 
-function prepareBaseClassForInjection () {
-  sed -i "s|&|\\\\&|g" $baseClassPath
-  tr '\n' '\t' < $baseClassPath > "$baseClassPath.new"
-  rm $baseClassPath
-  mv "$baseClassPath.new" $baseClassPath
+function escapeFile () {
+  cat $1 | jq -Rsc | sed "s|&|\\\\&|g" | sed 's/^\"//' | sed 's/\"$//'
+}
+
+function escapeLine () {
+  echo $1 | jq -Rsc | sed 's/^\"//' | sed 's/\"$//'
 }
 
 function fixClass () {
@@ -135,10 +142,7 @@ function fixClass () {
 
     # Inject base class to outer class
     extendedClassLine=$(cat $outerClassPath | grep "$extendedClass")
-    sed -i "s|$extendedClassLine|$(cat $baseClassPath)|g" $outerClassPath
-    tr '\t' '\n' < $outerClassPath > "$outerClassPath.new"
-    rm $outerClassPath
-    mv "$outerClassPath.new" $outerClassPath
+    sed -i "s|$extendedClassLine|$(escapeFile $baseClassPath)|g" $outerClassPath
   done
 }
 
@@ -155,6 +159,40 @@ function classBundlerMake () {
   sed -i "s|/\* bundler-make-private \*/ public|private|g" $outerClassPath
 }
 
+function prepareExternalTestClassForInjection () {
+  local classPath=$1
+  local className=$(echo $classPath | nameOfClass)
+  sed -i -E "/^@IsTest$/d" $classPath
+  sed -i -E "/^class $className \{$/d" $classPath
+  sed -i -E "/^\}$/d" $classPath
+  sed -i -E "s/static void ((\d|\w|_)+)/static void ${className}_\1/g" $classPath
+}
+
+function classBundlerInject () {
+  local outerClassPath=$1
+  local injectables=$2
+
+  # Check if class has 'bundler-inject' comment
+  (grep 'bundler-inject' $outerClassPath > /dev/null) || return 0
+
+  local bundlerInjectRegex="/\* bundler-inject $CLASS_REGEX \*/"
+
+  local targetClasses=$(egrep -o "$bundlerInjectRegex" $outerClassPath)
+  echo "$targetClasses" | while IFS= read -r targetClass; do
+
+    local targetClassName=$(echo "$targetClass" | egrep -o "$CLASS_REGEX")
+
+    # Error if base class is not in the $injectables directory
+    (echo "$injectables" | grep "$baseClassName" > /dev/null) \
+      || (echo "Class '$targetClassName' not found" > /dev/stderr ; exit 1)
+
+    local targetClassPath=$(echo "$injectables" | grep "$targetClassName.cls")
+
+    # Inject base class to outer class
+    sed -i -E "s/^  \/\* bundler-inject $targetClassName \*\/$/$(escapeFile $targetClassPath)/" $outerClassPath
+  done
+}
+
 function trimEmptyLines () {
   outerClassPath=$1
 
@@ -162,10 +200,22 @@ function trimEmptyLines () {
   sed -i "s/ *$//g" $outerClassPath
 }
 
+function beautify () {
+  local classPath=$1
+
+  sed -i '/^$/d' $classPath
+  sed -i -E "s/^  \}$/  }\n/g" $classPath
+  sed -i -E "s/^  [^ ].*;$/\0\n/g" $classPath
+}
+
 externals=$(scanExternals)
 externalsNames=$(echo "$externals" | nameOfClass)
 classes=$(scanClasses)
 classesNames=$(echo "$classes" | nameOfClass)
+testExternals=$(scanTestExternals)
+testExternalsNames=$(echo "$testExternals" | nameOfClass)
+testClasses=$(scanTestClasses)
+testClassesNames=$(echo "$testClasses" | nameOfClass)
 
 echo "$externalsNames" | while IFS= read -r line; do fixBaseClass $line; done
 echo "$classes" | while IFS= read -r line; do foreignExternalInitCalls $line; done
@@ -173,3 +223,10 @@ echo "$classes" | while IFS= read -r line; do fixClass $line; done
 echo "$classes" | while IFS= read -r line; do classBundlerMake $line; done
 echo "$classes" | while IFS= read -r line; do trimEmptyLines $line; done
 echo "$externals" | while IFS= read -r line; do rm $line; done
+echo "$testExternals" | while IFS= read -r line; do prepareExternalTestClassForInjection $line; done
+echo "$testClasses" | while IFS= read -r line; do classBundlerInject $line "$testExternals"; done
+echo "$testExternals" | while IFS= read -r line; do rm $line; done
+
+echo "$classes" | while IFS= read -r line; do [ -f $line ] && beautify $line ; done
+
+find './' -empty -type d -delete
